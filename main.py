d62ab334ea6bb571d5077d35b543f455ffceca2c
G1 <jeevanvaishnavv@gmail.com>
2022-02-02 17:16:45 +0530

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from multiprocessing import connection
 from kivymd.app import MDApp
 from kivy.lang import Builder
-import sqlite3
+import mysql.connector
 
 
 class SQLLite(MDApp):
@@ -10,61 +10,86 @@ class SQLLite(MDApp):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Lime"
 
-        # Create Database or Connect to one
-        con = sqlite3.connect("myfirstdb.db")
-        # Create a cursor
-        c = con.cursor()
+        # create database or connect to one
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user='root',
+            password="root",
+            database="second_db"
+        )
+        # create a cursor
+        c = mydb.cursor()
+        # create an actual database
+        c.execute("CREATE DATABASE if not exists second_db ")
+
+        # Check to see if database was created
+        c.execute('SHOW DATABASES')
+        for db in c:
+            print(db)
+        c.execute("USE second_db")
+
         # Create a table
-        c.execute("""
-            CREATE TABLE if not exists customers(
-                name text
-            )
-        """)
+        c.execute("CREATE TABLE if not exists customers(name VARCHAR(50))")
 
-        # Commit our changes
-        con.commit()
+        # # cHECK TO SEE IF TABLE CREATED
+        # c.execute("SELECT  * FROM customers")
+        # print(c.description)
+#
+        mydb.commit()
 
-        # Close the connection
-        con.close()
+        mydb.close()
 
         return Builder.load_file('main.kv')
 
     def fSubmit(self):
-        # Create Database or Connect to one
-        con = sqlite3.connect("myfirstdb.db")
-        # Create a cursor
-        c = con.cursor()
-        # Add A Record
-        c.execute("INSERT INTO customers VALUES (:first)", {
-            'first': self.root.ids.word_input.text,
-        })
+        # create database or connect to one
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user='root',
+            password="root",
+            database="second_db"
+        )
+        # create a cursor
+        c = mydb.cursor()
 
-        # Add a title msg
+        # add a records from customerss
+        sql_cmd = "INSERT INTO customers (name) VALUES (%s)"
+        values = (self.root.ids.word_input.text,)
+
+        # Execute th command
+        c.execute(sql_cmd, values)
+        # Add title in the message
         self.root.ids.word_label.text = f"{self.root.ids.word_input.text}"
-        # Clear in the title
-        self.root.ids.word_input.text = ""
-        # Commit our changes
-        con.commit()
 
-        # Close the connection
-        con.close()
+        # clear the input
+        self.root.ids.word_input.text = ""
+        mydb.commit()
+
+        mydb.close()
 
     def fShowDetails(self):
-        # Create Database or Connect to one
-        con = sqlite3.connect("myfirstdb.db")
-        # Create a cursor
-        c = con.cursor()
-        # Add A Record
-        c.execute("SELECT * FROM customers ")
+        # create database or connect to one
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user='root',
+            password="root",
+            database="second_db"
+        )
+        # create a cursor
+        c = mydb.cursor()
+
+        # Grab records from customerss
+        c.execute('SELECT * FROM customers')
+
         records = c.fetchall()
-
-        words = ""
+        word = ''
         for record in records:
-            words = f"{words}\n{record[0]}"
-            self.root.ids.word_label.text = f"{words}"
+            word = f"{word}\n{record[0]}"
+            self.root.ids.word_label.text = f"{word}"
 
-        con.commit()
-        con.close()
+        mydb.commit()
+
+        mydb.close()
 
 
 if __name__ == "__main__":
